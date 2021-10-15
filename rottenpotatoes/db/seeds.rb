64fb2ab9622fca36513cd16cb947af1d68be13b1
Ubuntu <ubuntu@ip-172-31-82-27.ec2.internal>
2021-10-15 02:16:49 +0000

    added hw3 files

--- rottenpotatoes/db/seeds.rb
+++ rottenpotatoes/db/seeds.rb
@@ -16,6 +16,10 @@ movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
       	  {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004'},
       	  {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'},
       	  {:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000'},
+      	  {:title => '12 Angry Men', :rating => 'G', :release_date => '21-Jun-1950'},
+      	  {:title => 'Fight Club', :rating => 'R', :release_date => '21-Jun-1999'},
+      	  {:title => 'Seven', :rating => 'R', :release_date => '21-Jun-1995'},
+      	  {:title => 'Pyscho', :rating => 'R', :release_date => '21-Jun-1960'},
   	 ]
 
 movies.each do |movie|
